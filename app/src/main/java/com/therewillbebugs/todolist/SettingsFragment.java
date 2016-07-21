0214e66252590f1ae26a6d8f85462279b5cb1bdd
PK <pdk07@my.sfu.edu>
2016-07-21 16:49:20 -0400

    Still null

--- app/src/main/java/com/therewillbebugs/todolist/SettingsFragment.java
+++ app/src/main/java/com/therewillbebugs/todolist/SettingsFragment.java
@@ -2,7 +2,6 @@ package com.therewillbebugs.todolist;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,34 +10,20 @@ import android.widget.RadioGroup;
 
 public class SettingsFragment extends android.support.v4.app.Fragment {
     public interface OnSettingsCompleteListener {
-        public void onSettingsComplete(boolean settingsSaved);
+        void onSettingsComplete(boolean settingsSaved);
     }
 
     public static final String TAG = "SettingsFragment";
+    public static final String NOTIFICATIONS_ENABLED_KEY = "NotificationsEnabled";
+    public static final String DEFAULT_PRIORITY_KEY = "DefaultPriority";
+
     private static boolean notificationsEnabled;
     private static Task.PRIORITY_LEVEL defaultPriorityLevel;
 
+    private View rootView;
     private OnSettingsCompleteListener listener;
     private RadioGroup defaultPriorityGroup;
     private CheckBox notificationsEnabledCheckbox;
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        View rootView = inflater.inflate(R.layout.settings_layout, container, false);
-
-        notificationsEnabledCheckbox = (CheckBox)rootView.findViewById(R.id.enable_notifications);
-        defaultPriorityGroup = (RadioGroup)rootView.findViewById(R.id.default_priority);
-
-        notificationsEnabled = notificationsEnabledCheckbox.isChecked();
-        defaultPriorityLevel = getSelectedPriorityLevel();
-
-        return rootView;
-    }
 
     @Override
     public void onAttach(Activity activity){
@@ -48,6 +33,46 @@ public class SettingsFragment extends android.support.v4.app.Fragment {
         } catch (ClassCastException e){
             throw new ClassCastException(activity.toString() + " must implement OnSettingsCompleteListener");
         }
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (savedInstanceState != null) {
+            notificationsEnabled = savedInstanceState.getBoolean(NOTIFICATIONS_ENABLED_KEY);
+            defaultPriorityLevel
+                    = Task.PRIORITY_LEVEL.get(savedInstanceState.getInt(DEFAULT_PRIORITY_KEY));
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        rootView = inflater.inflate(R.layout.settings_layout, container, false);
+
+        notificationsEnabledCheckbox = (CheckBox)rootView.findViewById(R.id.enable_notifications);
+        defaultPriorityGroup = (RadioGroup)rootView.findViewById(R.id.default_priority);
+
+        notificationsEnabled = notificationsEnabledCheckbox.isChecked();
+        defaultPriorityLevel = getSelectedPriorityLevel();
+        setRetainInstance(true);
+
+        return rootView;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState){
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putBoolean(
+            NOTIFICATIONS_ENABLED_KEY,
+            ((CheckBox)getView().findViewById(R.id.enable_notifications)).isChecked()
+        );
+
+        outState.putInt(DEFAULT_PRIORITY_KEY, defaultPriorityLevel.getVal());
     }
 
     public static boolean getNotificationsEnabled() {
